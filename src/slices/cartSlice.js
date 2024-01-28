9d793f3f045b4af1942e979755821c0bd313b545
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2024-01-28 17:22:24 +0530

    changes in cart slice and profile slice

--- src/slices/cartSlice.js
+++ src/slices/cartSlice.js
@@ -2,16 +2,14 @@ import { createSlice } from "@reduxjs/toolkit";
 // import {toast} from "react-hot-toast"
 
 const initialState = {
-    totalItems: localStorage.getItem("totalItems") ? JSON.parse(localStorage.getItem("totalItems")) : 0
+    cartItemsCount: localStorage.getItem('cartItemsCount') ? JSON.parse(localStorage.getItem('cartItemsCount')) : 0,
 };
 
 const cartSlice = createSlice({
-    name:"cart",
+    name:"cart slice",
     initialState:initialState,
     reducers:{
-        setTotalItems(state,value){
-            state.token = value.payload;
-        },
+        
     },
 });
 
