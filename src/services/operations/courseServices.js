2e3e535daa31c5b201ce5d200f1d1bf2422ab55f
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2024-02-04 13:15:02 +0530

    write function to fetch categories and edit Course

--- src/services/operations/courseServices.js
+++ src/services/operations/courseServices.js
@@ -1,8 +1,10 @@
 import toast from "react-hot-toast";
 import apiConnector from "../apiconnector";
-import { courseApi } from "../apis";
+import { categories, courseApi } from "../apis";
 
-const { POST_CREATE_COURSE_API } = courseApi;
+const { POST_CREATE_COURSE_API, PUT_EDIT_COURSE_API } = courseApi;
+
+const { CATEGORIES_API } = categories;
 
 export const addCourse = async (formData, token, dispatch, navigate) => {
   let result = null;
@@ -30,3 +32,40 @@ export const addCourse = async (formData, token, dispatch, navigate) => {
   toast.dismiss(toastId);
   return result;
 };
+
+export const editCourseDetails = async (
+  formData,
+  token,
+  dispatch,
+  navigate
+) => {
+  let result = null;
+
+  const toastId = toast.loading("Loading");
+  try {
+    const response = await apiConnector("PUT", PUT_EDIT_COURSE_API, formData, {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "multipart/form-data",
+    });
+
+    console.log(response.data);
+    result = response.data?.data;
+  } catch (error) {
+    toast.error(error?.response?.data?.error || "Cannot edit course,Try again");
+  }
+  toast.dismiss(toastId);
+  return result;
+};
+
+export const fetchCourseCategories = async (dispatch,navigate) => {
+    let result = [];
+    try{
+        const response = await apiConnector("GET",CATEGORIES_API);
+        console.log(response?.data?.data);
+        result = response.data?.data;
+    }
+    catch(error){
+        toast.error("Could not get course categories,try again");
+    }
+    return result;
+}
