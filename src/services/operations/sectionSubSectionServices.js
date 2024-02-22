fbf54696cb10311eee20455f692d977e3136dd80
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2024-02-22 14:03:27 +0530

    added delete section and subsection api's

--- src/services/operations/sectionSubSectionServices.js
+++ src/services/operations/sectionSubSectionServices.js
@@ -1,8 +1,9 @@
 import toast from "react-hot-toast";
 import { apiConnector } from "../apiconnector";
-import { sectionApi } from "../apis";
+import { sectionApi, subSectionApi } from "../apis";
 
-const { POST_CREATE_SECTION_API,PUT_UPDATE_SECTION_API } = sectionApi;
+const { POST_CREATE_SECTION_API,PUT_UPDATE_SECTION_API,DELETE_DELETE_SECTION_API } = sectionApi;
+const { DELETE_DELETE_SUBSECTION_API } = subSectionApi;
 
 export const updateSection = async (formData,token) => {
     let result = null;
@@ -42,6 +43,48 @@ export const createSection = async(formData,token) => {
     }
     catch(error){
         toast.error(error?.response?.data?.error || 'Cannot fetch course, Try Again')
+    }
+    toast.dismiss(toastId);
+    return result;
+};
+
+export const deleteSection = async (formData, token) => {
+    let result = null;
+
+    const toastId = toast.loading("Loading...");
+
+    try{
+        const response = await apiConnector("DELETE",DELETE_DELETE_SECTION_API,formData,{
+            'Content-Type': 'multipart/form-data',
+            Authorization: `Bearer ${token}`
+        });
+
+        result = response?.data?.data;
+        toast.success("Course Section Deleted");
+    }   
+    catch(error){
+        toast.error(error?.response?.data?.error || 'Cannot fetch course, Try Again');
+    }
+    toast.dismiss(toastId);
+    return result;
+};
+
+export const deleteSubSection = async(formData,token) => {
+    let result = null;
+
+    const toastId = toast.loading("Loading...");
+
+    try{
+        const response = await apiConnector("DELETE",DELETE_DELETE_SUBSECTION_API,formData,{
+            'Content-Type': 'multipart/form-data',
+            Authorization: `Bearer ${token}`
+        });
+
+        result = response?.data?.data;
+        toast.success("Lecture Deleted");
+    }
+    catch(error){
+        toast.error(error?.response?.data?.error || 'Cannot fetch course, Try Again');
     }
     toast.dismiss(toastId);
     return result;
