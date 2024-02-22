1c36162a7fd6a808a2f1f3ee51389223a5aef76d
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2024-02-22 14:03:00 +0530

    completed added delete section and subsection functionality

--- src/components/core/Dashboard/AddCourse/CourseBuilderForm/NestedView.jsx
+++ src/components/core/Dashboard/AddCourse/CourseBuilderForm/NestedView.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { BsListUl, BsFillCameraVideoFill } from "react-icons/bs";
 import { MdEdit } from "react-icons/md";
 import { RiDeleteBin6Line } from "react-icons/ri";
@@ -7,10 +7,16 @@ import { AiFillCaretRight, AiFillCaretDown } from "react-icons/ai";
 import { FaPlus } from "react-icons/fa";
 import SubSectionModal from "./SubSectionModal";
 import ConfirmationModal from "../../../../Common/ConfirmationModel";
+import { setCourse } from "../../../../../slices/addCourseSlice";
+import {
+  deleteSection,
+  deleteSubSection,
+} from "../../../../../services/operations/sectionSubSectionServices";
 
-const NestedView = () => {
+const NestedView = ({ handleChangeEditSectionName }) => {
   const { course } = useSelector((state) => state.addCourse);
 
+  const [loading, setLoading] = useState(false);
   const [listOfSectionOpen, setListOfSectionOpen] = useState([]);
 
   const [confirmationModalData, setConfirmationModalData] = useState(null);
@@ -18,13 +24,82 @@ const NestedView = () => {
   const [editSubSectionData, setEditSubSectionData] = useState([]);
   const [viewSubSectionData, setViewSubSectionData] = useState([]);
 
+  const { token } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+
+  const handleToggleListOpen = (sectionOpen, sectionId) => {
+    if (sectionOpen) {
+      setListOfSectionOpen(...listOfSectionOpen, sectionId);
+    } else {
+      setListOfSectionOpen(
+        listOfSectionOpen.filter((secId) => secId !== sectionId)
+      );
+    }
+  };
+
+  const handleDeleteSection = async (sectionId) => {
+    const result = await deleteSection({ sectionId }, token);
+    if (result) {
+      dispatch(setCourse(result));
+    }
+    setConfirmationModalData(null);
+  };
+
+  const handleOpenSectionDeleteModal = (sectionId) => {
+    setConfirmationModalData({
+      text1: "Delete this Section ?",
+      text2: "All the lectures in this section will be deleted",
+      btn1Text: "Delete",
+      btn2Text: "Cancel",
+      btn1Handler: () => handleDeleteSection(sectionId),
+      btn2Handler: () => setConfirmationModalData(null),
+      closeModalHandler: () => setConfirmationModalData(null),
+    });
+  };
+
+  const handleDeleteSubSection = async (subSectionId, sectionId) => {
+    setLoading(true);
+
+    const result = await deleteSubSection({ subSectionId }, token);
+
+    if (result) {
+      const updatedCourseSections = course.actions.map((section) =>
+        section._id === sectionId ? result : section
+      );
+
+      const updatedCourses = { ...course, sections: updatedCourseSections };
+
+      dispatch(setCourse(updatedCourses));
+    }
+    setLoading(false);
+    setConfirmationModalData(null);
+  };
+
+  const handleOpenSubSectionDeleteModal = (subSectionId, sectionId) => {
+    setConfirmationModalData({
+      text1: "Delete this Sub-Section ?",
+      text2: "This lecture will be deleted",
+      btn1Text: "Delete",
+      btn2Text: "Cancel",
+      btn1Handler: () => handleDeleteSubSection(subSectionId.sectionId),
+      btn2Handler: () => setConfirmationModalData(null),
+      closeModalHandler: () => setConfirmationModalData(null),
+    });
+  };
+
   return (
     <div>
       <div className="bg-richblack-700 rounded-lg py-6 px-8">
         {course &&
           course?.sections.map((section) => (
             <div key={section.id}>
-              <details open>
+              <details
+                open
+                onToggle={(e) =>
+                  handleToggleListOpen(e.target.open, section._id)
+                }
+              >
                 {/* Section Header Content  */}
                 <summary className="flex justify-between items-center cursor-pointer border-b-2 border-richblack-600 py-2">
                   <div className="flex items-center gap-x-3">
@@ -33,11 +108,16 @@ const NestedView = () => {
                   </div>
 
                   <div className="flex items-center gap-x-3">
-                    <button type="button">
+                    <button
+                      type="button"
+                      onClick={() =>
+                        handleChangeEditSectionName(section._id, section.title)
+                      }
+                    >
                       <MdEdit />
                     </button>
 
-                    <button>
+                    <button onClick={handleOpenSectionDeleteModal(section._id)}>
                       <RiDeleteBin6Line className="text-xl text-richblack-300" />
                     </button>
                     <span className="text-richblack-300 font-medium">|</span>
@@ -58,6 +138,7 @@ const NestedView = () => {
                   {section.subSection.map((subSection) => (
                     <div
                       key={subSection._id}
+                      onClick={() => setViewSubSectionData(subSection)}
                       className="flex justify-between items-center cursor-pointer gap-x-3 border-b-2 border-richblack-600 py-2"
                     >
                       <div className="flex items-center gap-x-3 py-2">
@@ -71,11 +152,21 @@ const NestedView = () => {
                         onClick={(e) => e.stopPropagation()}
                         className="flex items-center gap-x-3"
                       >
-                        <button type="button">
-                          <MdEdit />
+                        <button
+                          type="button"
+                          onClick={() => setEditSubSectionData(subSection)}
+                        >
+                          <MdEdit className="text-xl text-richblack-300" />
                         </button>
 
-                        <button>
+                        <button
+                          onClick={() =>
+                            handleOpenSubSectionDeleteModal(
+                              subSection._id,
+                              section._id
+                            )
+                          }
+                        >
                           <RiDeleteBin6Line />
                         </button>
                       </div>
@@ -85,9 +176,12 @@ const NestedView = () => {
                   {/* button to add new lecture  */}
                   <button
                     type="button"
+                    onClick={() =>
+                      setAddSubSectionData({ sectionId: section._id })
+                    }
                     className="mt-3 flex items-center gap-x-1 text-yellow-50"
                   >
-                    <FaPlus />
+                    <FaPlus className="text-lg" />
                     <p>Add lecture</p>
                   </button>
                 </div>
